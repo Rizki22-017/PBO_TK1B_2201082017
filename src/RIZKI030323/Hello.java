7b8a4cdf6605dff1b7d9485826cce17ace4c3a1b
Rizki22-017 <rizky211215@gmail.com>
2023-03-03 19:26:40 +0700

    RIZKI-Tugas 1 PBO TK 1B

---
+++ src/RIZKI030323/Hello.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package RIZKI030323;
+
+/**
+ *
+ * @author HMM
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
